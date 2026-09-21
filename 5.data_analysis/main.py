9f572d9a562534c1107c0ccb4bdda0db4cd70103
Marcos Paulo <marcos.pdo1412@gmail.com>
2026-09-21 20:11:11 -0300

    feat: mean, median, standart deviation and distribution by age

--- 5.data_analysis/main.py
+++ 5.data_analysis/main.py
@@ -2,22 +2,27 @@ import pandas
 import json
 from datetime import date
 
-data = pandas.read_csv("PESSOA_2018.TXT", delimiter=";", nrows=100)
+data = pandas.read_csv("PESSOA_2018.TXT", delimiter=";", nrows=10000, usecols=["CO_SEXO_PESSOA", "CO_RACA_COR_PESSOA", "DT_NASC_PESSOA"])
 df = pandas.DataFrame(data)
 
-# Load label configuration from json
 with open("label.json", encoding="utf-8") as file:
     config = json.load(file)
 
-    # Converts age delimiter to year delimiters based on current year
-    year = date.today().year
-    config["age_groups"]["delimiters"] = [
-        year - age
-        for age in config["age_groups"]["delimiters"]
-    ][::-1]
-
-    config["age_groups"]["labels"] = config["age_groups"]["labels"][::-1]
-
     df["DT_NASC_PESSOA"] = pandas.to_datetime(df["DT_NASC_PESSOA"])
 
+year = 2018
+df["age"] = year - df["DT_NASC_PESSOA"].dt.year
 
+df["age_groups"] = pandas.cut(
+    df["age"],
+    bins=config["age_groups"]["delimiters"],
+    labels=config["age_groups"]["labels"],
+    include_lowest=True
+)
+
+sex_race_groups = df.groupby(["CO_SEXO_PESSOA", "CO_RACA_COR_PESSOA"])
+
+print(sex_race_groups["age_groups"].value_counts())
+print(sex_race_groups["age"].mean())
+print(sex_race_groups["age"].median())
+print(sex_race_groups["age"].std())
