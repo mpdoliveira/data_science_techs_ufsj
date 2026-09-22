e99f38e0bb60a94e58d6e8630686a7dbbae91163
Marcos Paulo <marcos.pdo1412@gmail.com>
2026-09-22 11:14:34 -0300

    feat: initial comparation of work and age range

--- 5.data_analysis/main.py
+++ 5.data_analysis/main.py
@@ -2,36 +2,32 @@ import pandas
 import json
 from datetime import date
 
-def sex_race_age(data=None) :
-    if not data:
-        data = pandas.read_csv(
-            "PESSOA_2018.TXT",
-            delimiter=";",
-            nrows=10000,
-            usecols=[
-                "CO_SEXO_PESSOA",
-                "CO_RACA_COR_PESSOA",
-                "DT_NASC_PESSOA"
-            ]
-        )
+PATH = "PESSOA_2018.TXT"
+
+def df_cols(path=PATH):
+    data = pandas.read_csv(
+        path,
+        delimiter=";",
+        nrows=1,
+    )
+
+    return data.columns
+
+def sex_race_age(path=PATH, year=date.today().year) :
+    data = pandas.read_csv(
+        path,
+        delimiter=";",
+        nrows=10000,
+        usecols=[
+            "CO_SEXO_PESSOA",
+            "CO_RACA_COR_PESSOA",
+            "DT_NASC_PESSOA"
+        ]
+    )
 
     df = pandas.DataFrame(data)
-    
 
-    with open("label.json", encoding="utf-8") as file:
-        config = json.load(file)
-
-        df["DT_NASC_PESSOA"] = pandas.to_datetime(df["DT_NASC_PESSOA"])
-
-    year = 2018
     df["age"] = year - df["DT_NASC_PESSOA"].dt.year
-
-    df["age_groups"] = pandas.cut(
-        df["age"],
-        bins=config["age_groups"]["delimiters"],
-        labels=config["age_groups"]["labels"],
-        include_lowest=True
-    )
 
     sex_race_groups = df.groupby(["CO_SEXO_PESSOA", "CO_RACA_COR_PESSOA"])
 
@@ -40,21 +36,18 @@ def sex_race_age(data=None) :
     )
     print(agregation)
 
-    print(sex_race_groups["age_groups"].value_counts())
 
-
-def work_type(data=None):
-    if not data:
-        data = pandas.read_csv(
-            "PESSOA_2018.TXT",
-            delimiter=";",
-            nrows=10000,
-            usecols=[
-                "CO_CONCLUIU_FREQUENTOU_MEMB",
-                "CO_CURSO_FREQ_PESSOA_MEMB",
-                "VL_RENDA_BRUTA_12_MESES_MEMB"
-            ]
-        )
+def work_type(path=PATH):
+    data = pandas.read_csv(
+        path,
+        delimiter=";",
+        nrows=10000,
+        usecols=[
+            "CO_CONCLUIU_FREQUENTOU_MEMB",
+            "CO_CURSO_FREQ_PESSOA_MEMB",
+            "VL_RENDA_BRUTA_12_MESES_MEMB"
+        ]
+    )
 
     df = pandas.DataFrame(data)
 
@@ -65,5 +58,38 @@ def work_type(data=None):
 
     print(level_education["VL_RENDA_BRUTA_12_MESES_MEMB"].mean())
 
+def work_age_group(path=PATH, year=date.today().year):
+    data = pandas.read_csv(
+        path,
+        delimiter=";",
+        nrows=1000000,
+        usecols=[
+            "DT_NASC_PESSOA",
+            "VL_REMUNER_EMPREGO_MEMB"
+        ]
+    )
+
+    df = pandas.DataFrame(data)
+    df.dropna(inplace=True)
+
+    with open("label.json", encoding="utf-8") as file:
+        config = json.load(file)
+        df["DT_NASC_PESSOA"] = pandas.to_datetime(df["DT_NASC_PESSOA"])
+
+    df["age_groups"] = pandas.cut(
+        year - df["DT_NASC_PESSOA"].dt.year,
+        bins=config["age_groups"]["delimiters"],
+        labels=config["age_groups"]["labels"],
+        include_lowest=True
+    )
+
+    df["works"] = df["VL_REMUNER_EMPREGO_MEMB"] > 0
+
+    print(df.groupby(["age_groups","works"])["age_groups"].value_counts())
+
+    
+
 if __name__ == '__main__':
-    work_type()
+    #sex_race_age(year=2018)
+    #work_type()
+    work_age_group(year=2018)
